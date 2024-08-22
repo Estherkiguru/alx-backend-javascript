d0566d00f258aa7a3deccc3577587ee512243d12
EStherKiguru <esipraisewambui@gmail.com>
2024-08-22 23:39:44 +0300

    tests

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,31 +1,22 @@
 const assert = require("assert");
-const { it, describe } = require("mocha");
-const calculateNumber = require("./1-calcul");
-
-describe("calculateNumber()", function() {
-
-    it(`checking if numbers round`, function() {
-      const res = calculateNumber("SUM", 1, 2);
-      assert.strictEqual(res, 3);
+const calculateNumber = require('./1-calcul');
+describe("calculateNumber()", () => {
+    it(`should return 3 for inputs 1 and 2 for sum`, () => {
+      assert.strictEqual(calculateNumber("SUM", 1, 2), 3);
     });
-    it(`checking if numbers round`, function() {
-      const res = calculateNumber("SUBTRACT", 1.4, 2.2);
-      assert.strictEqual(res, -1);
+    it(`should return -1 for inputs 1.4, 2.2 subtraction`, () => {
+      assert.strictEqual(calculateNumber("SUBTRACT", 1.4, 2.2), -1);
     });
-    it(`checking if numbers round`, function() {
-      const res = calculateNumber("SUBTRACT", 4.9, 2.7);
-      assert.strictEqual(res, 2);
+    it(`should return 2 for inputs 4.9 , 2.7 subtraction`, () => {
+      assert.strictEqual(calculateNumber("SUBTRACT", 4.9, 2.7), 2);
     });
-    it(`checking if numbers round`, function() {
-      const res = calculateNumber("DIVIDE", 4, 2);
-      assert.strictEqual(res, 2);
+    it(`should return 2 for input 4, 2 division`, () => {
+      assert.strictEqual(calculateNumber("DIVIDE", 4, 2), 2);
     });
-    it(`checking if numbers round`, function() {
-      const res = calculateNumber("DIVIDE", 1.7, 0);
-      assert.strictEqual(res, "Error");
+    it(`should return Error for inputs 1.7, 0 division`, () => {
+      assert.strictEqual(calculateNumber("DIVIDE", 1.7, 0), "Error");
     });
-    it(`checking if numbers round`, function() {
-      const res = calculateNumber("DIVIDE", 1.4, 4.6);
-      assert.strictEqual(res, 0.2);
+    it(`should return 0.2 for inputs 1.4, 4.6`, () => {
+      assert.strictEqual(calculateNumber("DIVIDE", 1.4, 4.6), 0.2);
     });
 });
